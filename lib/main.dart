eec78ebc7eea39908fa12560ca539376b3be2f5d
joohyelim1208 <jhl01208@gmail.com>
2026-03-11 19:00:06 +0900

    feat: GoRoute 로직 분리 조인페이지에서 홈으로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+import 'package:han_ppyeom/core/router/app_router.dart';
 import 'package:han_ppyeom/core/theme/app_theme.dart';
-import 'package:han_ppyeom/ui/pages/home/home_page.dart';
-import 'package:han_ppyeom/ui/pages/join/join_page.dart';
 
-// 고라우터도 프로바이더로 관리
-final routerProvider = Provider<GoRouter>((ref) {
-  return GoRouter(
-    initialLocation: '/Join', // 테스트를 위해 우선 조인페이지 설정. '/'으로 변경하기.
-    routes: [
-      GoRoute(
-        path: '/home',
-        name: 'home',
-        builder: (context, state) => HomePage(),
-      ),
-      GoRoute(
-        path: '/join',
-        name: 'join',
-        builder: (context, state) => JoinPage(),
-      ),
-
-      // 파라미터가 필요한 경로가 있으면 추가. 페이지 이동 시 특정 데이터(ID, 이름, 카테고리 등) 함께 넘겨줘야 될 때
-    ],
-  );
-});
 void main() {
   // 리버팟 패키지
   runApp(ProviderScope(child: MyApp()));
